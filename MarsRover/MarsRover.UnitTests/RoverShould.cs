01b66f57a618f4df35cf27c4ecd992b5e66281e8
Dan Hunter <daniel.hunter@asos.com>
2023-07-07 11:50:37 +0100

    initial failing test

---
+++ MarsRover/MarsRover.UnitTests/RoverShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace MarsRover.UnitTests
+{
+    [TestFixture]
+    public class RoverShould
+    {
+        [Test]
+        public void Not_Be_Null()
+        {
+            // Arrange
+            var rover = new Rover();
+
+            //Act
+
+            //Assert
+            Assert.That(rover, Is.Not.Null);
+        }
+
+    }
+}
---
+++ MarsRover/MarsRover/Rover.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarsRover
+{
+    public class Rover
+    {
+        public Rover()
+        {
+            throw new NotImplementedException();    
+        }
+    }
+}
